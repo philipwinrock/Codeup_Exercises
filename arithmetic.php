acc37c59b2e60d05314309337fc08578d8c649e6
philip james winrock <philipjameswinrock@philips-MacBook-Pro.local>
2014-08-18 17:16:44 -0500

    exercise from class

--- arithmetic.php
+++ arithmetic.php
@@ -3,62 +3,105 @@
 
 
 
-//  // Add code here
-// function add($a, $b) {
-//     echo $a + $b . PHP_EOL;
-// }
-
-// function subtract($a, $b) {
-//     echo $a - $b . PHP_EOL;
-// }
-
-// function multiply($a, $b) {
-// 	echo $a * $b . PHP_EOL;
-
-// 		# code...
-	
-//     // Add code here
-// }
-
-// function divide($a, $b) {
-// 	echo $a / $b . PHP_EOL;
-//     // Add code here
-
-// }
-// function modulus($a,$b){
-// 	echo $a % $b . PHP_EOL;
-
-// }
-
-
-
-
-
-// 	add(21,2);
-// 	subtract(122,39);
-// 	multiply(99,12);
-// 	divide(123,14);
-// 	modulus(12,3);
-
-
-// 	$a=12.5; $b=19;
-
-// 	add($a,$b);
-// 	subtract($a,$b);
-// 	multiply($a,$b);
-// 	divide($a,$b);
-// 	modulus($a,$b);
-
-
-
-
+ // Add code here
 function add($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
-        echo $a + $b;
-    } else {
-        echo "ERROR: Both arguments must be numbers\n";
-    }
+	if(is_numeric($a) && is_numeric($b)){
+    echo $a + $b . PHP_EOL;
 }
+	else {
+		none_integer($a ,$b);
+}
+}
+function subtract($a,$b) {
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a - $b . PHP_EOL;
+}	else{
+		none_integer($a ,$b);
+
+}
+}
+		# code...
+
+    
+
+function multiply($a, $b) {
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a * $b . PHP_EOL;
+
+}
+	else {
+		
+		none_integer($a ,$b);
+}
+}
+		# code...
+	
+    // Add code here
+
+function divide($a, $b) {
+	if(is_numeric($a) && is_numeric($b)) {
+		if ($b!=0){
+
+		echo $a % $b . PHP_EOL;
+}
+	else{
+		divide_by_zero($a , $b);
+	}
+	
+}   // Add code here
+	else {
+		none_integer($a ,$b);
+
+}
+}
+function modulus($a,$b){
+	if(is_numeric($a) && is_numeric($b)) {
+		if ($b!=0){
+
+		echo $a % $b . PHP_EOL;
+}
+	else{
+		divide_by_zero($a , $b);
+	}
+}
+	else {
+		none_integer($a ,$b);
+}
+
+}
+
+
+Function divide_by_zero($a , $b){
+	echo '$a is ' . $a . ' and $b is ' . $b ."   ";
+	echo 'ERROR do not divide by Zero' . PHP_EOL;
+
+
+}
+Function none_integer($a , $b){
+	echo '$a is ' . $a . ' and $b is ' . $b ."   ";
+	echo 'ERROR  Non integer ' . PHP_EOL;
+}
+
+	add(1, 1);
+	subtract(12,39);
+	multiply(56,12);
+	divide(29,12);
+	modulus(12,3);
+
+
+	// $a=12.5; $b=19;
+
+	// add($a,$b);
+	// subtract($a,$b);
+	// multiply($a,$b);
+	// divide($a,$b);
+	// modulus($a,$b);
+
+
+
+
+
+
 
 
 
